f14aac680624310a7c9a7806992daf85b394d980
canssidle <canssidlewairimu@gmail.com>
2019-05-20 10:55:43 +0300

    initializing blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -17,13 +17,13 @@ def create_app(config_name):
     # config_options[config_name].init__app(app) 
 
 
-#     # initializing flask extention 
+    # initializing flask extention 
 
-#     bootstrap.init_app(app)
+    bootstrap.init_app(app)
 
-#     # registering the blueprint 
-#     from .main import main as main_blueprint
-#     app.register_blueprint(main_blueprint)
+    # registering the blueprint 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 #     #setting config 
 #     from .requests import configure_request
