b39b1b12967275f39364d12a3c9e888bf657609d
canssidle <canssidlewairimu@gmail.com>
2019-05-20 11:14:42 +0300

    set it to development mode

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from app import create_app
 from flask_script import Manager, Server
 
-# #creating app instance
+#creating app instance
 
-# app = create_app('development')
+app = create_app('development')
 
-# manager = Manager(app)
+manager = Manager(app)
 
 # manager.add_command('server',Server)
 
