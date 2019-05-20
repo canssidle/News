a855681cc16b9c139c183908793722b8448802bd
canssidle <canssidlewairimu@gmail.com>
2019-05-20 11:12:07 +0300

    importing the os

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# import os
+import os
 
-# class Config() :
-#     """
-#     Gereral configuration of the parent class
-#     """
-#     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
-#     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
+class Config() :
+    """
+    Gereral configuration of the parent class
+    """
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # class ProdConfig(Config) :
 #     """
