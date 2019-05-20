0e27619c81bf19ad26c771a0748cbb6e0f82a7bd
canssidle <canssidlewairimu@gmail.com>
2019-05-20 10:54:19 +0300

    initialising bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
-# #creating bootstrap instance 
+#creating bootstrap instance 
 
-# bootstrap = Bootstrap() 
+bootstrap = Bootstrap() 
 
-# def create_app(config_name):
+def create_app(config_name):
     
-#     #create flask app instance 
+    #create flask app instance 
     
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
 #     # Creating the app configurations 
 #     app.config.from_object(config_options[config_name])
