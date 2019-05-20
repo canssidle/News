9019696063717eea18779fc9435e066f7c7a4670
canssidle <canssidlewairimu@gmail.com>
2019-05-20 10:54:54 +0300

    initializing the config options

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,9 @@ def create_app(config_name):
     
     app = Flask(__name__)
 
-#     # Creating the app configurations 
-#     app.config.from_object(config_options[config_name])
-#     # config_options[config_name].init__app(app) 
+    # Creating the app configurations 
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init__app(app) 
 
 
 #     # initializing flask extention 
