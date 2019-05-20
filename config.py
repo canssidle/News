dc7bebf18508c3d09c826ed86ab0a3d336c9bba1
canssidle <canssidlewairimu@gmail.com>
2019-05-20 11:13:56 +0300

    set the development and production mode

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class DevConfig(Config) :
     """
     DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
